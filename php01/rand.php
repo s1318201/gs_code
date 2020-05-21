1e8e88a2fc985ad5ecdaa26f9accdf51ca08f0a1
asakawa <s1318201@cst.otsuma.ca.jp>
2020-05-21 18:58:36 +0900

    初回登録

---
+++ php01/rand.php
@@ -0,0 +1,39 @@
+<?php
+//$a ="山崎";
+//$b ="小山";
+//echo $a;
+//echo $b;
+
+$rand = rand(1,5);
+if($rand == 1){
+    echo "大吉";
+}
+if($rand == 2){
+    echo "中吉"；
+}
+if($rand == 3){
+    echo "小吉";
+}
+if($rand == 4){
+    echo "吉";
+}
+if($rand == 5){
+    scho "凶"
+}
+?>
+
+
+
+
+DOCTYPE html>
+<html lang="ja">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Document</title>
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+</body>
+</html>
