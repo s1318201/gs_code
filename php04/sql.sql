b1868a73bac1d14337329ac7dfd3de4a329b006d
asakawa <s1318201@cst.otsuma.ca.jp>
2020-05-29 16:41:30 +0900

    SQL_01

---
+++ php04/sql.sql
@@ -0,0 +1,19 @@
+INSERT INTO gs_an_table(id,name,email,age,naiyou,indate)VALUE(NULL,'test1','test1@test.jp',30,'test',sysdate());
+
+INSERT INTO gs_an_table(id,name,email,age,naiyou,indate)VALUE(NULL,'test2','test2@test.jp',40,'test',sysdate());
+
+INSERT INTO gs_an_table(id,name,email,age,naiyou,indate)VALUE(NULL,'test3','test3@test.jp',50,'test',sysdate());
+
+INSERT INTO gs_an_table(id,name,email,age,naiyou,indate)VALUE(NULL,'test4','test4@test.jp',60,'test',sysdate());
+
+INSERT INTO gs_an_table(id,name,email,age,naiyou,indate)VALUE(NULL,'test5','test5@test.jp',70,'test',sysdate());
+
+INSERT INTO gs_an_table(id,name,email,age,naiyou,indate)VALUE(NULL,'test6','test6@test.jp',80,'test',sysdate());
+
+INSERT INTO gs_an_table(id,name,email,age,naiyou,indate)VALUE(NULL,'test7','test7@test.jp',20,'test',sysdate());
+
+INSERT INTO gs_an_table(id,name,email,age,naiyou,indate)VALUE(NULL,'test8','test8@test.jp',30,'test',sysdate());
+
+INSERT INTO gs_an_table(id,name,email,age,naiyou,indate)VALUE(NULL,'test9','test9@test.jp',40,'test',sysdate());
+
+INSERT INTO gs_an_table(id,name,email,age,naiyou,indate)VALUE(NULL,'test10','test10@test.jp',50,'test',sysdate());
